62af4bea90b56513d61911a96f55484b7bba6ac7
Helder <he7d3r@gmail.com>
2014-09-20 11:29:50 -0300

    Remove tracking link (only useful on wiki)

--- src/DebugModeToggle.js
+++ src/DebugModeToggle.js
@@ -2,7 +2,6 @@
  * Add a toggle for debug mode to the sidebar
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Tools/DebugModeToggle.js]] ([[File:User:Tools/DebugModeToggle.js]])
  */
 /*jslint browser:true, white:true */
 /*global mediaWiki, jQuery */
